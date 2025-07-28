1f3112a5e69b74583575d60a8f0ad286f4682f76
ashikajnair <156599911+ashikajnair@users.noreply.github.com>
2025-07-28 22:37:58 +0530

    Update problem2_test.c

--- problem2_test.c
+++ problem2_test.c
@@ -11,12 +11,11 @@ void main() {
     int array_1[] = {0, 1, 0, 3, 12};
     int array_2[] = {1, 2, 3, 4};
     int array_3[] = {0, 0, 0, 0};
-    int array_4[] = {0, 1, 0, 4, 21};
 
-    int sizes[] = {5, 4, 4, 5};
-    int* tests[] = {array_1, array_2, array_3, array_4};
+    int sizes[] = {5, 4, 4};
+    int* tests[] = {array_1, array_2, array_3};
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 3; i++) {
 
         zeroes(tests[i], sizes[i]);
         printArray(tests[i], sizes[i]);
